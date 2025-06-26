19277097082a917fe7b1408e26883c3424b87bd0
Abdul Balhas <abdul.balhas2607@gmail.com>
2025-06-26 15:39:34 +0200

    feat(stadtserver): Stadtserver application Klasse hinzugefügt und pom.xml geändert

---
+++ stadtserver/src/main/java/de/stadtherne/StadtserverApplication.java
@@ -0,0 +1,11 @@
+package de.stadtherne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StadtserverApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StadtserverApplication.class, args);
+    }
+}
