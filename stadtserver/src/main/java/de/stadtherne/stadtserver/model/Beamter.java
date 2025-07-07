841361378dad2631bbbcf871a211f36945d4d634
luffy191919 <baturalpkalabalik99@gmail.com>
2025-07-07 21:12:21 +0200

    fix(stadtserver/model): beamter klasse änderungen

--- stadtserver/src/main/java/de/stadtherne/stadtserver/model/Beamter.java
+++ stadtserver/src/main/java/de/stadtherne/stadtserver/model/Beamter.java
@@ -1,48 +1,42 @@
 
 package de.stadtherne.stadtserver.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 @Entity
 @Data
+@EqualsAndHashCode(callSuper = true)
+@DiscriminatorValue("BEAMTER")
 public class Beamter extends Nutzer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    private String vorname;
-    private String name;
-    
-    @Embedded
-    private AnschriftT anschrift;
-    
     private String position;
 
-    public Beamter() {
-        this.veranstaltungen = new ArrayList<>();
-    }
-
     public Beamter(String vn, String n, AnschriftT a, String pos) {
-        this.vorname = vn;
-        this.name = n;
-        this.anschrift = a;
+        super(vn, n, a);
         this.position = pos;
         this.veranstaltungen = new ArrayList<>();
     }
-
     @OneToMany(mappedBy = "beamter", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Veranstaltung> veranstaltungen = new ArrayList<>();
 
     public void addVeranstaltung(Veranstaltung veranstaltung) {
-        veranstaltungen.add(veranstaltung);
-        veranstaltung.setBeamter(this);
+        if (veranstaltung != null) {
+            veranstaltungen.add(veranstaltung);
+            veranstaltung.setBeamter(this);
+        }
     }
 
     public void removeVeranstaltung(Veranstaltung veranstaltung) {
-        veranstaltungen.remove(veranstaltung);
-        veranstaltung.setBeamter(null);
+        if (veranstaltung != null) {
+            veranstaltungen.remove(veranstaltung);
+            veranstaltung.setBeamter(null);
+        }
     }
 }
