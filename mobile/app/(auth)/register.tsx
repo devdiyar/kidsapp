12d2ba320fd50f76879e8186a5c51bb021dc0fe4
luffy191919 <baturalpkalabalik99@gmail.com>
2025-07-10 14:49:38 +0200

    feat(mobile): Anmeldung Implementierung in Registrierung übernommen, umbau folgt gleich

--- mobile/app/(auth)/register.tsx
+++ mobile/app/(auth)/register.tsx
@@ -1,14 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import { StyleSheet, View, Text, Image } from "react-native";
+import AppButton from "@/components/ui/AppButton";
+import AppTextInput from "@/components/ui/AppTextInput";
+import { router } from "expo-router";
 
-const register = () => {
+export default function RegisterScreen() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <View>
-      <Text>register</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Register</Text>
+      <AppTextInput
+        placeholder="Benutzername/Email"
+        value={username}
+        onChangeText={setUsername}
+        autoCapitalize="none"
+      />
+      <AppTextInput
+        placeholder="Passwort"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <AppButton
+        title="Anmelden"
+        variant="orange"
+      />
+      <AppButton
+        title="Ein Konto erstellen"
+        onPress={() => router.push("/(auth)/register")}
+        variant="black"
+      />
+      <Text style={styles.orText}>Oder</Text>
+      <AppButton
+        title="Ohne Anmeldung fortfahren"
+        onPress={() => router.push("../(tabs)/search")}
+        variant="black"
+      />
     </View>
-  )
+  );
 }
 
-export default register
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 32,
+  },
+  orText: {
+    textAlign: "center",
+    marginVertical: 8,
+    fontSize: 15,
+  },
+});
