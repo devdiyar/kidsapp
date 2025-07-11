898e3cc9792893d214b91d8ffb948434f8df0fa7
diyardev001 <diyarhas.dev@gmail.com>
2025-07-11 17:19:41 +0200

    fix(mobile): Nun wird korrekt geroutet, nach der Anmeldung kommt man auf die Interaktivekarte

--- mobile/app/(profiletabs)/_layout.tsx
+++ mobile/app/(profiletabs)/_layout.tsx
@@ -1,35 +1,27 @@
-import { Stack } from 'expo-router';
+  import { Stack } from 'expo-router';
 
 export default function ProfileTabsLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: true }}>
       <Stack.Screen 
         name="favoriten" 
         options={{ 
-          title: 'Favoriten',
-          headerBackTitle: 'Zurück'
-        }} 
+        }}
       />
       <Stack.Screen 
         name="angemeldeteVeranstaltungen" 
         options={{ 
-          title: 'Angemeldete Veranstaltungen',
-          headerBackTitle: 'Zurück'
-        }} 
+        }}
       />
       <Stack.Screen 
         name="BesuchteVeranstaltungen" 
         options={{ 
-          title: 'Besuchte Veranstaltungen',
-          headerBackTitle: 'Zurück'
-        }} 
+        }}
       />
       <Stack.Screen 
         name="Einstellungen" 
         options={{ 
-          title: 'Einstellungen',
-          headerBackTitle: 'Zurück'
-        }} 
+        }}
       />
     </Stack>
   );
