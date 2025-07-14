306cc53e22c6d5c43cf4d92b60961b3297f47069
diyardev001 <diyarhas.dev@gmail.com>
2025-07-14 11:44:06 +0200

    fix(package-lock.json): Verwundbarkeiten behoben, Abhängigkeit-Version aktualisiert

--- mobile/src/config/api.ts
+++
@@ -1,8 +0,0 @@
-// const API = "http://localhost:8080/"; // Lokal
-const API = "http://10.0.2.2:8080/";  // Emulator
-
-export const API_ENDPOINTS = {
-  REGISTER: `${API}/{endpunkt}`,
-  LOGIN: `${API}/{endpunkt}`,
-  REFRESH_TOKEN: `${API}/{endpunkt}`,
-};
